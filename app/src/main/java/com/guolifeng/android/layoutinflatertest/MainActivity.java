e33db60d6fe2106ebf82f96f3df729c274cc2f09
guolifeng <glf_work@163.com>
2017-03-18 21:05:23 +0800

    1:LayoutInflater简单使用

--- app/src/main/java/com/guolifeng/android/layoutinflatertest/MainActivity.java
+++ app/src/main/java/com/guolifeng/android/layoutinflatertest/MainActivity.java
@@ -1,15 +1,25 @@
 package com.guolifeng.android.layoutinflatertest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
+
+    private LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainLayout = (LinearLayout) findViewById(R.id.main_layout);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View buttonLayout = layoutInflater.inflate(R.layout.button_layout, null);
+        mainLayout.addView(buttonLayout);
     }
+
 }
